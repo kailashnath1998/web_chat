dd295fbbeb77f0b242c90aa67f3699feca1a4850
kailashnath1998 <kailashnath1998@gmail.com>
2018-01-07 17:24:15 +0530

    'final'

--- Server/controller.js
+++ Server/controller.js
@@ -1,4 +1,4 @@
-var models = require('db.js');
+var models = require('./db.js');
 var path = require('path');
 var bodyParser = require('body-parser');
 
@@ -11,16 +11,18 @@ module.exports = function (app,io){
     }));
     
     app.get('/',function(req,res){
-        //code here
+        res.sendFile(path.resolve(__dirname+"/../views/index.html"));
     });
     
     app.post('/register',function(req,res){
         res.setHeader('Access-Control-Allow-Origin', '*');
         res.setHeader("Access-Control-Allow-Method","'GET, POST, OPTIONS, PUT, PATCH, DELETE'");
         var user={
-            /*
-				code here
-            */
+            "name":req.body.name,
+            "handle":req.body.handle,
+            "password":req.body.password,
+            "phone":req.body.phone,
+            "email":req.body.email,
         };
         console.log(user);
         
@@ -32,11 +34,11 @@ module.exports = function (app,io){
                 models.user.create(user,function(err,doc){
                     if(err) res.json(err);
                     else{
-                        // code here
+                        res.sendFile(path.resolve(__dirname+"/../views/test.html"));
                     }
                 });
             }else{
-                // code here
+                res.send("User already found");
             }
         })
         
@@ -50,9 +52,8 @@ module.exports = function (app,io){
     
     app.post('/login',function(req,res){
         console.log(req.body.handle);
-        /*
-			code here
-        */
+        res.setHeader('Access-Control-Allow-Origin', '*');
+        res.setHeader("Access-Control-Allow-Method","'GET, POST, OPTIONS, PUT, PATCH, DELETE'");
         handle = req.body.handle;
         models.user.findOne({"handle":req.body.handle, "password":req.body.password},function(err,doc){
             if(err){
@@ -63,7 +64,7 @@ module.exports = function (app,io){
             }
             else{
                 console.log("Asas"+__dirname);
-                // code here
+                res.send("success");
             }
             
     });
@@ -72,36 +73,17 @@ module.exports = function (app,io){
     io.on('connection',function(socket){
         console.log("Connection :User is connected  "+handle);
         console.log("Connection : " +socket.id);
+        
         io.to(socket.id).emit('handle', handle);
         users[handle]=socket.id;
         keys[socket.id]=handle;
+        
         console.log("Users list : "+users);
         console.log("keys list : "+keys);
-        models.user.find({"handle" : handle},{friends:1,_id:0},function(err,doc){
-            if(err){res.json(err);}
-            else{
-                friends=[];
-                pending=[];
-                all_friends=[];
-                console.log("friends list: "+doc);
-                list=doc[0].friends.slice();
-                console.log(list);
-                
-                /*
-					code here
-                */
-                console.log("pending list: "+pending);
-                console.log("friends list: "+friends);
-                /*
-					code here
-                */
-            }
-        });
-        
         
         socket.on('group message',function(msg){
             console.log(msg);
-            // code here
+            io.emit('group',msg);
         });
         
         socket.on('private message',function(msg){
@@ -115,115 +97,13 @@ module.exports = function (app,io){
         });
         
         socket.on('disconnect', function(){
-            /*
-				code here
-            */
+            delete users[keys[socket.id]];
+            delete keys[socket.id];
+            io.emit('users',users);
             console.log(users);
         });
     });
     
-    app.post('/friend_request',function(req,res){
-        /*
-			code here
-        */
-        friend=true;
-        models.user.find({"handle" : req.body.my_handle,"friends.name":req.body.friend_handle},function(err,doc){
-            if(err){res.json(err);}
-            else if(doc.length!=0){
-                console.log("Friend request : "+doc.length);
-                console.log("Friend request : friend request already sent "+doc);
-                // code here
-            }
-            else{
-                console.log("Friend request : "+doc.length);
-                models.user.update({
-                    // code here
-                },{
-                    $push:{
-                        friends:{
-                            name: req.body.friend_handle,
-                            status: // fill here
-                        }
-                    }
-                },{
-                    upsert:true
-                },function(err,doc){
-                    if(err){res.json(err);}
-                });
-                io.to(users[req.body.friend_handle]).emit('message', req.body);
-            }
-        });
-    });
     
-    app.post('/friend_request/confirmed',function(req,res){
-        console.log("friend request confirmed : "+req.body);
-        if(req.body.confirm=="Yes"){
-            models.user.find({
-                /*
-								code here
-                */
-            },function(err,doc){
-                if(err){
-                    // code here
-                }
-                else if(doc.length!=0){
-                    console.log("Friend request confirmed : "+doc.length);
-                    console.log("Friend request confirmed : friend request already sent "+doc);
-                    // code here
-                }
-                else{
-                    models.user.update({
-                        	/*
-								code here
-                            */
-                    },{
-                        '$set':{
-                            "friends.$.status":"Friend"
-                        }
-                    },function(err,doc){
-                        if(err){res.json(err);}
-                        else{
-
-                            console.log("friend request confirmed : Inside yes confirmed");
-                            /*
-								code here
-                            */
-                        }
-                    });
-                    models.user.update({
-                        handle:req.body.friend_handle
-                    },{
-                        $push:{
-                            friends:{
-                                /*
-									code here
-                                */
-                            }
-                        }
-                    },{upsert:true},function(err,doc){
-                        if(err){res.json(err);}
-                    });
-                }
-            });
-        }
-        else{
-            
-            console.log("friend request confirmed : Inside No confirmed");
-            models.user.update({
-                // code here
-            },{
-                '$pull':{
-                    'friends':{
-                        // code here
-                    }
-                }
-            },function(err,doc){
-            if(err){/* code here */}
-            else{
-                console.log("No");
-            }
-        });
-        }
-    });
     
 }
--- Server/db.js
+++ Server/db.js
@@ -1,38 +1,34 @@
-var mongoose = require(''); // fill inside ' '
+var mongoose = require('mongoose');
 
 var Schema = mongoose.Schema;
 
-mongoose.connect("  "); // Type url for connection
+mongoose.connect("mongodb://localhost:27017/chat");
 
 mongoose.connection.on('open', function (ref) {
     console.log('Connected to mongo server.');
 });
-
 mongoose.connection.on('error', function (err) {
     console.log('Could not connect to mongo server!');
     console.log(err);
 });
 
-mongoose.connect(' '); // Type url for connection
+mongoose.connect('mongodb://localhost/mongodb');
 
 module.exports.user=mongoose.model('User',new Schema({
     name:String,
-    /*
-        fill here
-    */
+    handle: String,
+    password: String,
+    phone:String,
+    email:String,
     friends:[]
 },{strict: false}));
-
 module.exports.online=mongoose.model('online',new Schema({
-    /*
-        code here
-    */
+    handle:String,
+    connection_id:String
 }));
-
 module.exports.messages=mongoose.model('message',new Schema({
     message : String,
-    /*
-        fill here
-    */
+    sender  : String,
+    reciever: String,
     date    : Date
 }));
